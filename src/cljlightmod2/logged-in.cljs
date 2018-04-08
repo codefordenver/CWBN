3eeffbeb014d76d8ca91bf8a0c6ce93942b4f78d
KBHan <kbhanlive@gmail.com>
2018-04-08 12:40:11 -0600

    update logged-in

--- src/cljlightmod2/logged-in.cljs
+++ src/cljlightmod2/logged-in.cljs
@@ -1,5 +1,31 @@
 (ns cljlightmod2.logged-in
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [cljlightmod2.submit :as submit]
+            
+            [cljs.reader :refer [read-string]]))
 
-(defn logged-in-component []
-  [:div "logged-in Component"])
+
+(defn add-entity []
+  [:div [submit/submit-content]])
+
+(defn update-entity[]
+  [:div "Update-one"])
+
+(defn delete-entity[]
+  [:div "Delete-one"])
+
+(defn view-all []
+  [:div "view"]) 
+
+(defn edit-component []
+  [:div 
+   [:div [add-entity]]
+   [:div [update-entity]]
+   [:div [delete-entity]]
+   [:div [view-all]]])
+   
+
+
+(defn logged-in-component[]
+  [:div "logged-in-component"
+   [:div [edit-component]]])
