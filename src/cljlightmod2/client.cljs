40453c7cb9005b068676e995d7484b5793834c54
KBHan <kbhanlive@gmail.com>
2018-04-08 15:17:24 -0600

    update variable name logged-in -> logged-in-status

--- src/cljlightmod2/client.cljs
+++ src/cljlightmod2/client.cljs
@@ -3,13 +3,13 @@
             [reagent.core :as r]
             [cljlightmod2.submit :as submit]
             [cljlightmod2.menubar :as menu]
-            [cljlightmod2.menubar :refer [logged-in]]
+            [cljlightmod2.menubar :refer [logged-in-status]]
             [cljlightmod2.logged-out :as logout-comp]
             [cljlightmod2.logged-in :as login-comp]))
 
 ; conditional component depending on log-in status
 (defn conditional-component []
-  (if (= @logged-in "true")
+  (if (= @logged-in-status "true")
     [login-comp/logged-in-component]
     [logout-comp/logged-out-component]))
   
--- src/cljlightmod2/menubar.cljs
+++ src/cljlightmod2/menubar.cljs
@@ -1,11 +1,11 @@
 (ns cljlightmod2.menubar
   (:require [reagent.core :as r]))
 
-(def logged-in (r/atom "false"))
+(def logged-in-status (r/atom "false"))
 
 (defn logged-in-selection []
-  [:select {:value @logged-in
-            :on-change #(reset! logged-in (-> % .-target .-value))}
+  [:select {:value @logged-in-status
+            :on-change #(reset! logged-in-status (-> % .-target .-value))}
    [:option {:value true} "logged-in"]
    [:option {:value false} "logged-out"]])
 
@@ -18,11 +18,11 @@
 (defn logged-out-menubar []
   [:div [generic-menubar] [logged-in-selection]])
 
-(defn what-menubar-to-show [logged-in]
-  (if (= logged-in "true")
+(defn what-menubar-to-show [logged-in-status]
+  (if (= logged-in-status "true")
     (logged-in-menubar)
     (logged-out-menubar)))
 
 (defn menubar []
   [:div 
-   [:p [what-menubar-to-show @logged-in]]])
+   [:p [what-menubar-to-show @logged-in-status]]])
