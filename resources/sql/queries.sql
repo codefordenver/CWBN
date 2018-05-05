0987542e6cb0c87bd20454548dbf514ec26cd859
KBHan <kbhanlive@gmail.com>
2018-05-05 11:32:22 -0600

    Modified queries.sql, Tested get-org, create-org, delete-org successfully in server repl.

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,21 @@
 -- :name create-org! :! :n
 -- :doc creates a new organization record
 INSERT INTO cwbn
-(id, name, type, address, support_type, timestamp)
-VALUES (:id, :name, :type, :address, :support_type, :timestamp)
+(name, type, address, support_type, timestamp)
+VALUES (:name, :type, :address, :support_type, :timestamp)
 
 -- :name update-org! :! :n
--- :doc updates an existing user record
+-- :doc updates an existing org record
 UPDATE cwbn
 SET name = :name, type = :type, address = :address, support_type = :support_type, timestamp = :timestamp
 WHERE id = :id
 
 -- :name get-org :? :1
--- :doc retrieves a user record given the id
+-- :doc retrieves an org record given the id
 SELECT * FROM cwbn
 WHERE id = :id
 
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
+-- :name delete-org! :! :n
+-- :doc deletes an org record given the id
 DELETE FROM cwbn
 WHERE id = :id
--- src/clj/cwbn/db/core.clj
+++ src/clj/cwbn/db/core.clj
@@ -6,18 +6,7 @@
     [cwbn.config :refer [env]]))
 
 (defstate ^:dynamic *db*
-          :start(conman/connect!)
-            {:datasource
-              (doto (org.h2.jdbcx.JdbcDataSource.)
-                    (.setURL (env :database-url))
-                    (.setUser "")
-                    (.setPassoword ""))}
-          :stop (conman/disconnect! *db*))
-
-
-(comment
-  (defstate ^:dynamic *db*
             :start (conman/connect! {:jdbc-url (env :database-url)})
-            :stop (conman/disconnect! *db*)))
+            :stop (conman/disconnect! *db*))
 
 (conman/bind-connection *db* "sql/queries.sql")
