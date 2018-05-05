47b8adac222877251fc69a8c4310208a5d9e319d
KBHan <kbhanlive@gmail.com>
2018-05-05 09:49:46 -0600

    Set-up data layer - queries.sql updated

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,21 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name create-org! :! :n
+-- :doc creates a new organization record
+INSERT INTO cwbn
+(id, name, type, address, support_type, timestamp)
+VALUES (:id, :name, :type, :address, :support_type, :timestamp)
 
--- :name update-user! :! :n
+-- :name update-org! :! :n
 -- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
+UPDATE cwbn
+SET name = :name, type = :type, address = :address, support_type = :support_type, timestamp = :timestamp
 WHERE id = :id
 
--- :name get-user :? :1
+-- :name get-org :? :1
 -- :doc retrieves a user record given the id
-SELECT * FROM users
+SELECT * FROM cwbn
 WHERE id = :id
 
 -- :name delete-user! :! :n
 -- :doc deletes a user record given the id
-DELETE FROM users
+DELETE FROM cwbn
 WHERE id = :id
