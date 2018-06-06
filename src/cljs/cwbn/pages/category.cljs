a055dcb47251a69ab048f7388909f47fcc85f0cb
eemshi <oreneemshi@gmail.com>
2018-06-05 18:36:59 -0600

    create org component, hard-code business development orgsfor demo, update font-weights

--- src/cljs/cwbn/pages/category.cljs
+++ src/cljs/cwbn/pages/category.cljs
@@ -2,6 +2,39 @@
   (:require [re-frame.core :as rf]
             [clojure.string :as s]))
 
+(defn org [name
+           type
+           services
+           population
+           area
+           website
+           contact-name
+           email
+           phone]
+  [:div {:class "org mb4"}
+   [:h2 {:class "f4 fw6 ttc mt1"} name]
+   [:h3.f6 [:i type]]
+   [:h3.f6.mb0 [:b "Services: "]
+               services]
+   (when-not (nil? population)
+     [:h3.f6.mb0 [:b "Target Population: "]
+                 population])
+   (when-not (nil? area)
+     [:h3.f6.mb0 [:b "Area Served: "]
+                 area])
+   (when-not (nil? website)
+     [:h3.f6 [:b "Website: "]
+             [:a {:href website}
+                 website]])
+   (when-not (nil? contact-name)
+     [:h3.f6.mb0 [:b "Contact: "]
+                 contact-name])
+   (when-not (nil? email)
+     [:h3.f6.mb0 [:a {:href (str "mailto:" email)} email]])
+   (when-not (nil? population)
+     [:h3.f6.mb0 phone])])
+
+
 (defn category-page []
   (let [services-by-category @(rf/subscribe [:services-by-category])
         category-route @(rf/subscribe [:category-route])
@@ -9,35 +42,101 @@
         category-img (str (first (s/split category-route #"-")) ".png")
         category-services (services-by-category (keyword category-route))]
     [:div.category-page
-     [:div {:class "category-header flex"}
+     [:div {:class "category-header flex items-center"}
       [:img {:class "category-icon"
              :src (str "img/category-icons/" category-img)}]
       [:div {:class "category-services flex flex-column"}
-       [:h1 {:class "f3 b ttc mt1"} category-name]
+       [:h1 {:class "f3 b ttc"} category-name]
        [:div
         (for [service category-services]
           ^{:key (gensym "service-")}
-          [:span {:class "service-link"}
+          [:span {:class "service-link fw6"}
            [:a {:href category-route} service]])]]]
-     [:div {:class "category-listings mt4"}
-      (when (= category-route "business-development")
-        [:div {:class "flex"}
-         [:div {:class "letter f3 b mr4 mr5-ns"} "M"]
-         [:div
-          [:h2 {:class "f5 b ttc mt1"}
-           "Mi Casa Resource Center"]
-          [:h3.f6 [:i "Non-Profit"]]
-          [:h3.f6 [:b "Services: "]
-                  "Business Technical Assistance, Lending"]
-          [:h3.f6 [:b "Target Population: "]
-                  "Entrepreneurs, women, Latinos"]
-          [:h3.f6 [:b "Area Served: "]
-                  "Denver Metro"]
-          [:h3.f6 [:b "Website: "]
-                  [:a {:href "micasaresourcecenter.org"}
-                   "micasaresourcecenter.org"]]
-          [:h3.f6.mt3 [:b "Contact: "]
-                      "Elena Vasconez"]
-          [:h3.f6 [:a {:href (str "mailto:" "evasconez@MiCasaResourceCenter.org")}
-                      "evasconez@MiCasaResourceCenter.org"]]
-          [:h3.f6 "(303) 573-1302"]]])]]))
+
+     ; hard-coded for demo purposes
+     (when (= category-route "business-development")
+       [:div {:class "category-listings mt4 pt1"}
+        [:div {:class "orgs-by-letter flex"}
+         [:div {:class "letter f3 b tl tc-ns w-20 w-10-l"} "A"]
+         [:div {:class "orgs w-80 w-90-l"}
+          [org "Accion"
+               "Non-Profit"
+               "Business Technical Assistance, Lending"
+               nil
+               nil
+               nil
+               nil
+               nil
+               nil]]]
+        [:div {:class "orgs-by-letter flex"}
+         [:div {:class "letter f3 b tl tc-ns w-20 w-10-l"} "C"]
+         [:div {:class "orgs w-80 w-90-l"}
+          [org "Colorado Enterprise Fund"
+               "Non-Profit"
+               "Business Lending"
+               nil
+               nil
+               nil
+               nil
+               nil
+               nil]
+          [org "Colorado Lending Source"
+               "Non-Profit"
+               "Business Lending"
+               nil
+               nil
+               nil
+               nil
+               nil
+               nil]
+          [org "Community Enterprise Development Services (CEDS)"
+               "Non-Profit"
+               "Technical Assistance, Microloans, IDAs"
+               "Refugees, entrepreneurs, consumers"
+               "Metro Area"
+               "scorecard.prosperitynow.org/2016"
+               "Bishal Kafle"
+               "bkafle@cedsfinance.org"
+               "(303) 569-8165"]]]
+        [:div {:class "orgs-by-letter flex"}
+         [:div {:class "letter f3 b tl tc-ns w-20 w-10-l"} "M"]
+         [:div {:class "orgs w-80 w-90-l"}
+          [org "Mi Casa Resource Center"
+               "Non-Profit"
+               "Business Technical Assistance, Lending"
+               "Entrepreneurs, women, Latinos"
+               "Denver Metro"
+               "micasaresourcecenter.org"
+               "Elena Vasconez"
+               "evasconez@MiCasaResourceCenter.org"
+               "(303) 573-1302"]]]
+        [:div {:class "orgs-by-letter flex"}
+         [:div {:class "letter f3 b tl tc-ns w-20 w-10-l"} "R"]
+         [:div {:class "orgs w-80 w-90-l"}
+          [org "Rocky Mountain Employee Ownership Center"
+               "Non-Profit"
+               "Technical Assistance"
+               "Business seeking to transition into an employee-owned business model"
+               "Colorado"
+               "rmeoc.org"
+               "Halisi Vinson"
+               "halisi@rmeoc.org"
+               nil]
+          [org "Rocky Mountain Farmers Union - Cooperative Development Center"
+               "Non-Profit"
+               "Technical Assistance for Coop Development"
+               "Urban areas"
+               "Denver"
+               "rmfu.org"
+               "Bill Stevenson"
+               "bill.stevenson@rmfu.org"
+               nil]
+          [org "Rocky Mountain Microfinance Institute"
+               "Non-Profit"
+               "Microloans, Technical Assistance"
+               "Entrepreneurs"
+               nil
+               nil
+               nil
+               nil
+               nil]]]])]))
--- src/cljs/cwbn/pages/home.cljs
+++ src/cljs/cwbn/pages/home.cljs
@@ -9,12 +9,12 @@
     [:div {:class "category-link w-20-l w-30-m tc mv3"}
      [:a {:href (str "/#/services/" slug)}
       [:img {:src (str "img/category-icons/" img-name ".png")
-             :class "w-40 w-34-ns mb2"}]
+             :class "w-40 w-30-ns mb2"}]
       [:p {:class "fw7 ttu f6 lh-solid mt1"} label]]]))
 
 (defn home-page []
   [:div
-   [:h1.tc.f2.fw7 "Let's build a stronger local economy"]
+   [:h1.tc.f2.fw6.mt4.mb3 "Let's build a stronger local economy"]
    [search-bar/component]
    [:div {:class "category-links flex flex-wrap justify-between"}
     [category-link "Financial Investment"]
