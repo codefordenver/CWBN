5f221f1c19db42e5978ff34416d9868dfdc0c0bb
annhorton <39928970+annhorton@users.noreply.github.com>
2018-06-28 20:33:59 -0600

    Fix lein repl timeout (#43)
    Revised project.clj to extend timeout so that lein repl will not timeout

--- project.clj
+++ project.clj
@@ -129,7 +129,8 @@
                   :doo {:build "test"}
                   :source-paths ["env/dev/clj"]
                   :resource-paths ["env/dev/resources"]
-                  :repl-options {:init-ns user}
+                  :repl-options {:init-ns user
+                                 :timeout 120000}
                   :injections [(require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
    :project/test {:jvm-opts ["-Dconf=test-config.edn"]
