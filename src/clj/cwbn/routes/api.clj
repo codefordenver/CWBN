54b79f9bc4003b4d4d234fe48ccecc6df59aeb11
David A. Viramontes <dviramontes@gmail.com>
2018-07-10 20:16:15 -0600

    spike: wip - adds dependencies and test route: http://localhost:3000/api/foobar

--- src/clj/cwbn/routes/api.clj
+++ src/clj/cwbn/routes/api.clj
@@ -1,3 +1,14 @@
 (ns cwbn.routes.api
-  (:require [taoensso.carmine :as car :refer (wcar)]))
-pr
+  (:require [compojure.core :refer [defroutes GET context]]
+            [ring.util.http-response :as response]
+            [taoensso.carmine :as car :refer [wcar]]))
+
+(def server1-conn {:pool {} :spec {:uri "redis://127.0.0.1:6379"}})
+
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+
+(prn (wcar* (car/ping)))
+
+(defroutes api-routes
+           (context "/api" []
+             (GET "/foobar" [] '(prn (wcar* (car/ping))))))
