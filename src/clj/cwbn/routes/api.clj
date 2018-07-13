5f724f62e04654719696892bed14267c94d92276
David A. Viramontes <dviramontes@gmail.com>
2018-07-12 18:27:50 -0600

    spike: wip - define airtable data model constant

--- src/clj/cwbn/routes/api.clj
+++ src/clj/cwbn/routes/api.clj
@@ -6,15 +6,22 @@
 
 (def AIRTABLE_API_KEY (System/getenv "AIRTABLE_API_KEY"))
 
-(def airtable-endpoint "https://api.airtable.com/v0/appIy3ycDv8Xf4dR3/Categories")
+(def airtable-records "Airtable's Domain model"
+  {::root          "https://api.airtable.com/v0/appIy3ycDv8Xf4dR3" ;; root api domain
+   ::organizations "/Organizations"
+   ::categories    "/Categories"
+   ::services      "/Services"
+   ::types         "/Types"
+   ::tags          "/Tags"})
 
 (def server1-conn {:pool {} :spec {:uri "redis://127.0.0.1:6379"}})
 
 (defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
 
-(defn- get-airtable-data []
+(defn- get-airtable-data [endpoint]
   (let [headers {:headers {"Authorization" (str "Bearer " AIRTABLE_API_KEY)}}
-        {:keys [status headers body error] :as resp} @(http/get airtable-endpoint headers)]
+        {:keys [status headers body error] :as resp}
+        @(http/get (str (::tags airtable-records) endpoint) headers)]
     (if error
       (println "Failed, exception: " error)
       (do
@@ -22,8 +29,13 @@
         (clojure.pprint/pprint body)))))
 
 (defn redis-handler [_]
-  (let [ping (wcar* (car/ping))]
-    (get-airtable-data)
+  (let [ping (wcar*
+               (car/ping)
+               (car/set "foo" "bar")
+               (car/get "foo"))]
+    (prn ping)
+    ; => ["PONG" "OK" "bar"] (3 commands -> 3 replies)
+    ;(get-airtable-data airtable-endpoint)
     (response/ok {:ok ping})))
 
 (defroutes api-routes
