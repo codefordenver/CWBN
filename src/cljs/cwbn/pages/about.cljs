667740361f15c4d433a0f8d9ead1d5aa5a1e4d0b
Patrick MacMurchy <pmm4654@gmail.com>
2018-09-24 19:32:33 -0600

    Proper margin/spacing

--- src/cljs/cwbn/pages/about.cljs
+++ src/cljs/cwbn/pages/about.cljs
@@ -10,8 +10,8 @@
       [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pulvinar ex. Maecenas hendrerit gravida purus. Nulla urna mi, egestas ut augue sit amet, rhoncus luctus quam. Curabitur eu convallis metus. Sed sapien magna, ultrices in orci ac, sollicitudin varius neque. Donec eget quam eu sem gravida accumsan. Duis ut egestas justo.
 Ut at nibh elementum, volutpat purus vel, varius felis. Aliquam gravida nunc at aliquam gravida. Fusce ut metus magna. Aliquam eget felis sollicitudin, posuere massa nec, imperdiet metus. Nunc porttitor ipsum ac orci bibendum egestas. Cras eget elit posuere, facilisis ante vel, cursus risus. Nulla eu libero a tortor convallis efficitur. Duis ac dictum ipsum."]
     ]
-    [:div.tc.ma1.mt4.ph3]    
-    [:div.tc.ma1.mt5.ph3
+    [:div.mt4]    
+    [:div.tc.mt5.ph3.mb1
       [:p.mb1 "Contact us"]
       [:a {:href "mailto:connect@communitywealthbuilding.org"} "connect@communitywealthbuilding.org"]
     ]    
