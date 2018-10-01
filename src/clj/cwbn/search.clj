bfd53f1b687ebbc396087fe6e8930a44186b4b9a
David A. Viramontes <dviramontes@gmail.com>
2018-09-30 18:36:44 -0600

    feat: org search (#97)
    feat: org search endpoint

--- src/clj/cwbn/search.clj
+++ src/clj/cwbn/search.clj
@@ -4,8 +4,6 @@
             [clojurewerkz.elastisch.rest.document :as doc]
             [clojurewerkz.elastisch.query :as q]))
 
-(def conn (atom nil))
-
 (def index-name "cwbn")
 
 (def _type "organizations")
@@ -14,34 +12,19 @@
 
 (defn setup []
   (try
-    (let [connection (esr/connect elastic-search-endpoint)]
-
-      (reset! conn connection)
-
-      (idx/delete @conn index-name)
-      (idx/create @conn index-name)
-
-      (doc/create @conn index-name _type {:fields  {:name "Article 1"}
-                                          :date    "2015-12-13T14:11:19"
-                                          :message "Testing"})
-      (doc/create @conn index-name _type {:fields  {:name "Article 1"}
-                                          :date    "2015-04-13T14:11:19"
-                                          :message "Test"})
-      (doc/create @conn index-name _type {:fields  {:name "Third"}
-                                          :date    "2015-11-13T14:12:19"
-                                          :message "Other"}))
+    (let [conn (esr/connect elastic-search-endpoint)]
+      (idx/delete conn index-name)
+      (idx/create conn index-name))
     (catch Exception e
       (throw e))))
 
 (defn search [query]
-  ((comp (partial map :_source) :hits :hits)
-   (doc/search @conn index-name _type {:query (q/match "fields.name" query)})))
+  (let [conn (esr/connect elastic-search-endpoint)]
+    ((comp (partial map :_source) :hits :hits)
+     (doc/search conn index-name _type {:query (q/match "fields.name" query)}))))
 
-
-
-
-
-
-
-
-
+(defn index [data]
+  (let [conn (esr/connect elastic-search-endpoint)]
+    (doall
+      (for [d data]
+        (doc/create conn index-name _type d)))))
