a214b6bf892fdabc41b607e852892bd0507f196f
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-10-20 15:46:47 -0600

    Updates search results to have exact result, and list other results below in alphabetical format. TODO: only show exact match when exact match exists

--- src/cljs/cwbn/components/details.cljs
+++ src/cljs/cwbn/components/details.cljs
@@ -10,36 +10,35 @@
                          contact-name
                          email
                          phone-number]}]
-  (fn []
-    [:div {:class "org mb4 pt2"}
-     [:h2 {:class "f4 fw6 ttc underline"}
-      (if website
-        [:a {:href website :target "_blank"} name]
-        name)]
-     (for [t type]
-       ^{:key (gensym)}
-       [:h3.f6 [:i t]])
-     (when-not (empty? services)
-       [:h3.f6.mb0
-        [:b "Services: "]
-        (for [s services
-              :let [i (.indexOf services s)
-                    comma? (when (> (count services) (+ 1 i)) ", ")]]
-          ^{:key (gensym)}
-          [:span (str s comma?)])])
-     (when status
-       [:h3.f6.mb0.pv1 [:b "Status: "] status])
-     (when population
-       [:h3.f6.mb0.pv1 [:b "Target Population: "] population])
-     (when area
-       [:h3.f6.mb0.pv1 [:b "Area Served: "] area])
-     (when website
-       [:h3.f6 [:b "Website: "]
-        [:a {:href website :target "_blank"} website]])
-     (when contact-name
-       [:h3.f6.mb0.pv1 [:b "Contact: "] contact-name])
-     (when email
-       [:h3.f6.mb0.pv1 [:a {:href (str "mailto:" email)} email]])
-     (when phone-number
-       [:h3.f6.mb0.pv1
-        [:a {:href (str "tel:" phone-number)} phone-number]])]))
+  [:div {:class "org mb4 pt2"}
+   [:h2 {:class "f4 fw6 ttc underline"}
+    (if website
+      [:a {:href website :target "_blank"} name]
+      name)]
+   (for [t type]
+     ^{:key (gensym)}
+     [:h3.f6 [:i t]])
+   (when-not (empty? services)
+     [:h3.f6.mb0
+      [:b "Services: "]
+      (for [s services
+            :let [i (.indexOf services s)
+                  comma? (when (> (count services) (+ 1 i)) ", ")]]
+        ^{:key (gensym)}
+        [:span (str s comma?)])])
+   (when status
+     [:h3.f6.mb0.pv1 [:b "Status: "] status])
+   (when population
+     [:h3.f6.mb0.pv1 [:b "Target Population: "] population])
+   (when area
+     [:h3.f6.mb0.pv1 [:b "Area Served: "] area])
+   (when website
+     [:h3.f6 [:b "Website: "]
+      [:a {:href website :target "_blank"} website]])
+   (when contact-name
+     [:h3.f6.mb0.pv1 [:b "Contact: "] contact-name])
+   (when email
+     [:h3.f6.mb0.pv1 [:a {:href (str "mailto:" email)} email]])
+   (when phone-number
+     [:h3.f6.mb0.pv1
+      [:a {:href (str "tel:" phone-number)} phone-number]])])
--- src/cljs/cwbn/pages/search_results.cljs
+++ src/cljs/cwbn/pages/search_results.cljs
@@ -2,14 +2,17 @@
   (:require reagent.core
             [re-frame.core :as rf]
             [cwbn.components.search-bar :as search-bar]
-            [cwbn.components.details :as details]))
+            [cwbn.components.details :as details]
+            [cwbn.components.sorted-list :as sorted-list]))
 
 (defn search-results-page []
-  (let [search-results (rf/subscribe [:search-results])]
+  (let [search-results @(rf/subscribe [:search-results])]
     [:div
      [search-bar/component]
-     [:h2.ph4 "Search Results"]
+     [:h2.ph4 "Top result"]
      [:div.ph4
-      (for [result @search-results]
-        ^{:key (gensym "result-")}
-        [details/component result])]]))
+      [details/component (first search-results)]]
+     (when (> (count search-results) 1)
+       [:div
+        [:h2.ph4 "Other results"]
+        [sorted-list/component (drop 1 search-results)]])]))
