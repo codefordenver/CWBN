33b4f060befc143e80e8307e4f694782bbc702ab
eemshi <oreneemshi@gmail.com>
2018-10-22 19:06:47 -0600

    category tooltip wip

--- src/cljs/cwbn/pages/home.cljs
+++ src/cljs/cwbn/pages/home.cljs
@@ -1,15 +1,29 @@
 (ns cwbn.pages.home
   (:require [re-frame.core :as rf]
+            [reagent.core :as rg]
             [clojure.string :as s]
+            [re-com.core :as re-com :refer-macros [handler-fn]]
+            [re-com.popover]
             [cwbn.components.search-bar :as search-bar]))
 
 (defn category-link [{:keys [label slug image]}]
-  [:div {:class "category-link w-20-l w-30-m tc mv3"}
-   [:a {:href (str "/#/category/" slug)}
-    [:img {:src (str "img/category-icons/" image )
-           :alt label
-           :class "w-40 w-30-ns mb2"}]
-    [:p {:class "fw7 ttu f6 lh-solid mt1"} label]]])
+  (let [showing? (rg/atom false)]
+    [:div {:class "category-link w-20-l w-30-m tc mv3"}
+     [re-com/popover-anchor-wrapper
+      :showing? showing?
+      :position :below-right
+      :anchor
+        [:a {:href (str "/#/category/" slug)
+             :on-mouse-over (handler-fn (reset! showing? true))
+             :on-mouse-out (handler-fn (reset! showing? false))}
+         [:img {:src (str "img/category-icons/" image)
+                :alt label
+                :class "w-40 w-30-ns mb2"}]
+         [:p {:class "fw7 ttu f6 lh-solid mt1"} label]]
+      :popover
+        [re-com/popover-content-wrapper
+         :close-button? false
+         :body "Click for more info"]]]))
 
 (defn home-page []
   (let [categories (rf/subscribe [:categories])
